d4af68fc534e35c8547e37c6ea6e825e59dd4f3b
indusahu <indusahu.npc@gmail.com>
2026-03-27 22:11:16 +0530

    Add dashboard route

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ function App() {
 
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
